47a5ffd3f9d8c56130aaf239741787609ac09a34
Lauren Dove <elledoveinc@gmail.com>
2019-09-23 02:13:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,7 +12,8 @@ def sort_array_char_count(arraystrings)
 end
 
 def swap_elements (array)
- array [0],  array [1] , array[2] = array[0], array[2] , array[1]
+  array [1] , array[2] =  array[2] , array[1]
+  array
 end
 
 
