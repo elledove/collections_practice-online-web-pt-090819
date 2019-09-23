6811d8067c212b90b958927f48f105d342b610bc
Lauren Dove <elledoveinc@gmail.com>
2019-09-23 02:06:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(arrays)
+  arrays.sort.reverse
+end
+
+def sort_array_char_count(arraystrings)
+
+   arraystrings.sort_by {|x| x.length}
+end
+
+def swap_elements (array)
+ array [0],  array [1] , array[2] = array[0], array[2] , array[1]
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new = []
+  array.each do |x|
+  new << x[2] = "$"
+  end
+end
+
+def find_a(array)
+ array.select do |x|
+ x.start_with?("a")
+ end
+end
+
+def sum_array(array)
+  array.inject do |x|
+  x.
+ end
+end
+
+
+
+
+def add_s
+end
 
